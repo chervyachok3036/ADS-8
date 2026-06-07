52a557c9e89ac83336b2fa30e7f6d36d56ac2761
chervyachok3036 <casha13113@gmail.com>
2026-06-07 20:18:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-
 #include <iostream>
 #include <string>
+
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
@@ -12,12 +12,12 @@ int main() {
 
   makeTree(tree, "src/war_peace.txt");
 
-  std::cout << "Tree depth:   " << tree.Depth() << "\n";
-  std::cout << "Unique words: " << tree.Size()  << "\n";
+  std::cout << "Tree depth:   " << tree.depth() << "\n";
+  std::cout << "Unique words: " << tree.size()  << "\n";
 
-  std::cout << "\nFrequency of 'war':   " << tree.Search("war")   << "\n";
-  std::cout << "Frequency of 'peace': " << tree.Search("peace") << "\n";
-  std::cout << "Frequency of 'the':   " << tree.Search("the")   << "\n";
+  std::cout << "\nFrequency of 'war':   " << tree.search("war")   << "\n";
+  std::cout << "Frequency of 'peace': " << tree.search("peace") << "\n";
+  std::cout << "Frequency of 'the':   " << tree.search("the")   << "\n";
 
   std::cout << "\nWords sorted by frequency:\n";
   printFreq(tree);
